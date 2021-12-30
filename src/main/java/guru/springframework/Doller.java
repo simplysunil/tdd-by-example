90f2f1afc78a32ff4c7585060dc5cfa9fae0f20f
Sunil <sunilit01@gmail.com>
2021-12-30 12:48:13 +0530

    New TDD Test Code added

--- src/main/java/guru/springframework/Doller.java
+++ src/main/java/guru/springframework/Doller.java
@@ -1,16 +1,45 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Doller {
-	int amount;
+	private int amount;
 	
     public Doller(int amount) {
         this.amount = amount;
     }
     
-    void times(int multiple) {
-        amount = multiple*2;
+    Doller times(int multiple) {
+        //amount = amount*multiple;
+    	return new Doller(amount*multiple);
+    	
     }
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doller other = (Doller) obj;
+		return amount == other.amount;
+	}
+
+	/*
+	public boolean equals(Object obj) {
+		//return true;
+		Doller doller = (Doller) obj;
+		return amount == doller.amount;
+	}
+	*/
+    
     
     
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,5 +1,6 @@
 package guru.springframework;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,15 @@ public class MoneyTest {
 	@Test
 	void moneyTest() {
 		Doller five = new Doller(5);
-        five.times(2);
-        System.out.println(five.amount);
-        assertEquals(4, five.amount);
+        Doller product = five.times(2);
+        assertEquals(new Doller(10), product);
+        product = five.times(3);
+        assertEquals(new Doller(15), product);
+	}
+	
+	@Test
+	void testEquality() {
+		assertEquals(new Doller(5), new Doller(5));
+		assertNotEquals(new Doller(5), new Doller(8));
 	}
 }
